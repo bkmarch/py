3dcaa16e4c5661798b0e0dba11d0b80dd0a7abae
bkmarch <bkap27@gmail.com>
2024-12-04 17:30:08 -0600

    Update serializers.py

--- api_model/serializers.py
+++ api_model/serializers.py
@@ -2,16 +2,6 @@ from rest_framework import serializers
 from database.models import Executive, EquityAward
 
 from api_model.models import EquityAwardValue
-
-class ExecutiveDeserializer(serializers.ModelSerializer):
-    class Meta:
-        model = Executive
-        fields = '__all__'
-
-class EquityAwardDeserializer(serializers.ModelSerializer):
-    class Meta:
-        model = EquityAward
-        fields = '__all__'
     
 class EquityAwardValueSerializer(serializers.ModelSerializer):
 
